63d4c76d4d6aa5b195928a6a5a1baec4c6a4096a
cira-w <2907571276@qq.com>
2026-03-30 22:11:36 +0800

    chat rename remove

--- components/sidebar/chat-list.tsx
+++ components/sidebar/chat-list.tsx
@@ -3,24 +3,24 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 import { useParams } from "next/navigation";
+import { ChatBox } from "./chat-box";
 export const ChatList = () => {
-  const chats = useQuery(api.chats.list);
-  const { chatId } = useParams<{ chatId: Id<"chats"> }>();
+    const chats = useQuery(api.chats.list);
+    const { chatId } = useParams<{ chatId: Id<"chats"> }>();
 
-  if (chats === undefined) {
-    return <div>Loading...</div>;
-  }
+    if (chats === undefined) {
+        return <div>Loading...</div>;
+    }
 
-  return (
-    <div>
-      {chats.map((chat) => (
-        <div
-          key={chat._id}
-          // className={chat._id === chatId ? "bg-gray-200" : ""}
-        >
-          {chat.title}
+    return (
+        <div className="flex flex-col flex-1 overflow-y-auto">
+            {chats.map((chat) => (
+                <ChatBox
+                    key={chat._id}
+                    chat={chat}
+                    selected={chat._id === chatId}
+                />
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
--- convex/chats.ts
+++ convex/chats.ts
@@ -1,51 +1,66 @@
 import { handler } from "next/dist/build/templates/app-page";
 import { mutation, query } from "./_generated/server";
-
+import { v } from "convex/values";
 export const create = mutation({
-  args: {},
-  //ctx=>context, args=>arguments
-  handler: async (ctx, args) => {
-    const identity = await ctx.auth.getUserIdentity();
-    if (!identity) {
-      throw new Error("Not authenticated");
-    }
-    const user = await ctx.db
-      .query("users")
-      .withIndex("by_token", (q) =>
-        q.eq("tokenIdentifier", identity.tokenIdentifier),
-      )
-      .unique();
-    if (!user) {
-      throw new Error("User not found");
-    }
-    const chatId = await ctx.db.insert("chats", {
-      userId: user._id,
-      title: "New Chat",
-    });
-    return chatId;
-  },
+    args: {},
+    //ctx=>context, args=>arguments
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+        const user = await ctx.db
+            .query("users")
+            .withIndex("by_token", (q) =>
+                q.eq("tokenIdentifier", identity.tokenIdentifier),
+            )
+            .unique();
+        if (!user) {
+            throw new Error("User not found");
+        }
+        const chatId = await ctx.db.insert("chats", {
+            userId: user._id,
+            title: "New Chat",
+        });
+        return chatId;
+    },
 });
 export const list = query({
-  args: {},
-  handler: async (ctx) => {
-    const identity = await ctx.auth.getUserIdentity();
-    if (!identity) {
-      throw new Error("Not authenticated");
-    }
-    //用户是否存在于数据库中
-    const user = await ctx.db
-      .query("users")
-      .withIndex("by_token", (q) =>
-        q.eq("tokenIdentifier", identity.tokenIdentifier),
-      )
-      .unique();
-    if (user === null) {
-      throw new Error("User not found");
-    }
-    //查询用户的聊天记录
-    return ctx.db
-      .query("chats")
-      .withIndex("by_userId", (q) => q.eq("userId", user._id))
-      .collect();
-  },
+    args: {},
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+        //用户是否存在于数据库中
+        const user = await ctx.db
+            .query("users")
+            .withIndex("by_token", (q) =>
+                q.eq("tokenIdentifier", identity.tokenIdentifier),
+            )
+            .unique();
+        if (user === null) {
+            throw new Error("User not found");
+        }
+        //查询用户的聊天记录
+        return ctx.db
+            .query("chats")
+            .withIndex("by_userId", (q) => q.eq("userId", user._id))
+            .collect();
+    },
+});
+
+export const rename = mutation({
+    args: { id: v.id("chats"), title: v.string() },
+    handler: async (ctx, args) => {
+        await ctx.db.patch(args.id, {
+            title: args.title,
+        });
+    },
+});
+export const remove = mutation({
+    args: { id: v.id("chats") },
+    handler: async (ctx, args) => {
+        await ctx.db.delete(args.id);
+    },
 });
